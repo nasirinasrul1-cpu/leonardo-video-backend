32683bcc3d24183840fa71f6036d1e9827c44b6a
nasirinasrul1-cpu <nasiri.nasrul1@gmail.com>
2026-05-15 20:06:19 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1489,9 +1489,11 @@ app.use((error, req, res, next) => {
 });
 
 if (require.main === module) {
+  if (require.main === module) {
   app.listen(PORT, () => {
     console.log(`AI Creator Studio backend aktif di http://localhost:${PORT}`);
   });
+}
 }
 
 module.exports = app;
